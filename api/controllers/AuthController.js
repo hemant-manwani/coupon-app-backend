e355328b13cbd5b5cf346da40e14894a6b982afe
Hemant <hemantmanwani.iitr@gmail.com>
2017-01-11 21:26:55 +0530

    Add sign in controller at /auth/login

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -6,27 +6,21 @@
  */
 
 module.exports = {
-  index: function (req, res) {
+  login: function (req, res) {
     var email = req.param('email');
     var password = req.param('password');
 
-    if (!email || !password) {
-      return res.json(401, {err: 'email and password required'});
-    }
+    if (!email || !password) return res.json(401, {err: 'email and password required'});
 
     User.findOne({email: email}, function (err, user) {
-      if (!user) {
-        return res.json(401, {err: 'invalid email or password'});
-      }
+
+      if (!user) return res.json(401, {err: 'invalid email or password'});
 
       User.comparePassword(password, user, function (err, valid) {
-        if (err) {
-          return res.json(403, {err: 'forbidden'});
-        }
-
-        if (!valid) {
+        if(err) return res.json(403, {err: 'forbidden'});
+        if(!valid) {
           return res.json(401, {err: 'invalid email or password'});
-        } else {
+        }else{
           res.json({
             user: user,
             token: jwToken.issue({id : user.id })
