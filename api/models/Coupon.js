42bbee4e6b9e37d791a30622bbd25cf1d862b24f
Hemant <hemantmanwani.iitr@gmail.com>
2017-01-11 21:17:21 +0530

    Parse coupon method in coupon models

--- api/models/Coupon.js
+++ api/models/Coupon.js
@@ -5,6 +5,7 @@
  * @docs        :: http://sailsjs.org/#!documentation/models
  */
 
+var _ = require('lodash');
 
 module.exports = {
 
@@ -44,5 +45,15 @@ module.exports = {
       redeem_by: {
         type: 'string'
       }
+	},
+
+	parseCoupon: function(coupon){
+		var data = _.clone(coupon, true);
+		var fields = ["amount_off", "duration_in_months", "max_redemptions", "percent_off"];
+		for(var x = 0; x < fields.length; x++){
+			if(coupon[fields[x]] != undefined)
+				data[fields[x]] = parseInt(coupon[fields[x]]);
+		}
+		return data;
 	}
 };
