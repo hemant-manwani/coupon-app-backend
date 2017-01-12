8153da18c9eda326d210368dd1a52d6d70dd29ca
Hemant <hemantmanwani.iitr@gmail.com>
2017-01-12 17:05:25 +0530

    Added description to Coupon/User models

--- api/models/Coupon.js
+++ api/models/Coupon.js
@@ -1,35 +1,34 @@
-/**
- * Users.js
- *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
- * @docs        :: http://sailsjs.org/#!documentation/models
- */
+	/**
+	* Users.js
+	*
+	* @description :: Coupon schema and model functions.
+	* @docs        :: http://sailsjs.org/#!documentation/models
+	*/
 
-var _ = require('lodash');
+	var _ = require('lodash');
 
-module.exports = {
+	module.exports = {
+		schema: true,
 
-	schema: true,
-
-	attributes: {
-	    id:{
+		attributes: {
+			id:{
 				type: 'string'
-      },
-      coupon_id: {
-        type: 'string'
-      },
-      user_id: {
-        type: 'string'
-      }
-   },
+			},
+			coupon_id: {
+				type: 'string'
+			},
+			user_id: {
+				type: 'string'
+			}
+		},
 
-	parseCoupon: function(coupon){
-		var data = _.clone(coupon, true);
-		var fields = ["amount_off", "duration_in_months", "max_redemptions", "percent_off"];
-		for(var x = 0; x < fields.length; x++){
-			if(coupon[fields[x]] != undefined)
+		parseCoupon: function(coupon){
+			var data = _.clone(coupon, true);
+			var fields = ["amount_off", "duration_in_months", "max_redemptions", "percent_off"];
+			for(var x = 0; x < fields.length; x++){
+				if(coupon[fields[x]] != undefined)
 				data[fields[x]] = parseInt(coupon[fields[x]]);
+			}
+			return data;
 		}
-		return data;
-	}
-};
+	};
--- api/models/User.js
+++ api/models/User.js
@@ -1,7 +1,7 @@
 /**
  * Users.js
  *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @description :: User schema and model functions..
  * @docs        :: http://sailsjs.org/#!documentation/models
  */
 
